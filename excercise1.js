491e123b4043286d84d9e89313e92f457ba91341
Tasneema Nolen <tasneemamnolen@gmai.com>
2022-10-17 11:03:18 -0400

    excercise 1+2

---
+++ excercise1.js
@@ -0,0 +1,33 @@
+"use strict";
+var customerId = "1100356984";
+var firstMiddleLastName = "Tasneema Maryam Nolen";
+var gender = "Female";
+var dateOfBirth = "12/31/2003";
+var driversLicenseNumber = "9999";
+var autoPolicyNumber = "N/A";
+
+console.log(
+  customerId,
+  firstMiddleLastName,
+  gender,
+  dateOfBirth,
+  driversLicenseNumber,
+  autoPolicyNumber
+);
+
+var phrase1, phrase2, wholePhrase;
+phrase1 = "Learning to program ";
+phrase2 = "is fun!";
+wholePhrase = phrase1 + phrase2;
+// wholePhrase contains "Learning to program is fun!"
+
+console.log(wholePhrase);
+
+var myName, myCity, myState;
+myName = " Tasneema Nolen";
+myCity = "Union City";
+myState = "Georgia";
+
+console.log(
+  "My name is " + myName + ". " + "I live in " + myCity + ", " + myState + "."
+);
---
+++ excericse2.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// sample inputs
+var foodCost = 79.25;
+var tax = 6.54;
+var tip = 12.0;
+// calculation
+var totalDue = foodCost + tax + tip;
+// output
+console.log("The total due is " + totalDue);
+
+
